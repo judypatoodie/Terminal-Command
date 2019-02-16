3199f0e4ecb8906b7a03106a23335fa869731878
judy <judy@Users-MacBook-Pro-4.local>
2019-02-15 18:20:42 -0800

    Added Terminal-commands from checkpoint

---
+++ terminal-commands.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+module.exports.ls = () => {
+  fs.readdir('./', (err, files) => {
+    const filesToString = files.reduce((acc, file) => {
+      return `${acc} ${file} `;
+    }, '');
+console.log(filesToString);
+  });
+};
+module.exports.touch = () => {
+  // your code here
+};
+module.exports.mkdir = () => {
+  // your code here
+};
+
